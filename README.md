cf37e30872916b46d200c33f00ae1b1510eeb02d
calvinatian <48587962+calvinatian@users.noreply.github.com>
2022-06-06 01:02:20 -0400

    Update README and pad guesses with whitespace.

--- README.md
+++ README.md
@@ -1,14 +1,122 @@
+# Doggle
+
 Playing around with [Dog API](https://dog.ceo/dog-api/) for code swap event.
 
-Ideas
+Guess the breed of the random dog image.
 
-* Dog Wordle (Doggle)
-  * Get a random picture of a dog. Try to guess the breed.
-  * Enhancements
-    * Choose which breeds to guess
-    * Parallax to images
+## How to play. 
 
-do row by row checking
-dont show how many letters it is
-  give clues/hints on how many letters over/under
-  red tiles/ghost tiles
+* Only guess main breed. (e.g Boston Terrier -> Terrier)
+* Just type directly on the page: don't select the text box.
+* Green - letter is in word and in correct position
+* Yellow - letter is in word but not in correct position (No duplicate handling yet. For example, if you type a word with two `e`'s and both are not in the correct position, both tiles will be yellow even though there is only one `e` in the answer)
+* Gray - letter is not in the word
+
+## Future improvements
+
+* Mobile version (virtaul keyboard or some other input method)
+* Choose which breeds to guess
+* Choose whether to guess full breed name or just the main breed name
+* Option to not show how many letters are in the answer
+  * give clues/hints on how many letters over/under
+  * red tiles/ghost tiles
+
+## Breeds:
+<!-- <details>
+<summary>Breeds:</summary> -->
+* Affenpinscher
+* African
+* Airedale
+* Akita
+* Appenzeller
+* Australian
+* Basenji
+* Beagle
+* Bluetick
+* Borzoi
+* Bouvier
+* Boxer
+* Brabancon
+* Briard
+* Buhund
+* Bulldog
+* Bullterrier
+* Cattledog
+* Chihuahua
+* Chow
+* Clumber
+* Cockapoo
+* Collie
+* Coonhound
+* Corgi
+* Cotondetulear
+* Dachshund
+* Dalmatian
+* Dane
+* Deerhound
+* Dhole
+* Dingo
+* Doberman
+* Elkhound
+* Entlebucher
+* Eskimo
+* Finnish
+* Frise
+* Germanshepherd
+* Greyhound
+* Groenendael
+* Havanese
+* Hound
+* Husky
+* Keeshond
+* Kelpie
+* Komondor
+* Kuvasz
+* Labradoodle
+* Labrador
+* Leonberg
+* Lhasa
+* Malamute
+* Malinois
+* Maltese
+* Mastiff
+* Mexicanhairless
+* Mix
+* Mountain
+* Newfoundland
+* Otterhound
+* Ovcharka
+* Papillon
+* Pekinese
+* Pembroke
+* Pinscher
+* Pitbull
+* Pointer
+* Pomeranian
+* Poodle
+* Pug
+* Puggle
+* Pyrenees
+* Redbone
+* Retriever
+* Ridgeback
+* Rottweiler
+* Saluki
+* Samoyed
+* Schipperke
+* Schnauzer
+* Setter
+* Sheepdog
+* Shiba
+* Shihtzu
+* Spaniel
+* Springer
+* Stbernard
+* Terrier
+* Tervuren
+* Vizsla
+* Waterdog
+* Weimaraner
+* Whippet
+* Wolfhound
+<!-- </details> -->
--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// // Basic blueprint
-// fecth(url)
-//   .then(response.something) // Define response type (JSON, Headers, Status codes)
-//   .then(data); // get the response type
-
+// This is some real bad code. Good luck to whoever gets this lol.
 const request = async () => {
   const response = await fetch("https://dog.ceo/api/breeds/image/random");
   const dogData = await response.json();
@@ -15,7 +11,6 @@ const request = async () => {
     const breedMainRe = /breeds\/(\w*)-?.*\//;
     const breedFull = dogData.message.match(breedFullRe)[1].toLowerCase();
     const breedMain = dogData.message.match(breedMainRe)[1].toLowerCase();
-    console.log(breedMain);
     data.set('breedFull', breedFull);
     data.set('breedMain', breedMain);
     data.set('dogImage', dogData.message)
@@ -26,20 +21,14 @@ const request = async () => {
   }
 };
 
-// request();
-
 const allBreeds = async () => {
   const response = await fetch("https://dog.ceo/api/breeds/list/all");
   const data = await response.json();
-  console.log(data);
   // do some error handling if data.status is not "success"
   if (data.status == "success") {
-    // console.log(data.message);
     document.getElementById("all-breeds").innerHTML=JSON.stringify(data, null, 4);
   }
 };
-
-// allBreeds();
 
 // function to perform a get request
 function httpGet(theUrl)
@@ -75,53 +64,33 @@ function getAllBreeds()
 }
 
 // gets a list of all breeds
-// var breeds = getAllBreeds();
+var breeds = getAllBreeds();
 
-// console.log(breeds);
-// console.log(String(breeds))
-// console.log(breeds.length)
-// console.log(typeof(breeds))
-// console.log(Object.values(breeds))
-// console.log(JSON.stringify(breeds));
 
 // document.getElementById("all-breeds").innerHTML=Object.values(breeds).toString();
 
-const log = document.querySelector(".event-log-contents");
-const reload = document.querySelector("#reload");
-
-reload.addEventListener("click", () => {
-  log.textContent = "";
-  window.setTimeout(() => {
-    window.location.reload(true);
-  }, 200);
-});
 
 window.addEventListener("load", (event) => {
-  log.textContent = log.textContent + "load\n";
+  // log.textContent = log.textContent + "load\n";
   async function simulate() {
     const data = await request();
-    console.log(data.get('img'))
     document.getElementById("answer").textContent=data.get('breedMain');
     document.getElementById("dogImage").src=data.get('dogImage');
-    let answers = Array(data.get('breedMain').length).fill(" ");
-    console.log(answers)
+    // let answers = Array(data.get('breedMain').length).fill(" ");
+    // console.log(answers)
 
     const game = document.getElementById("game");
     const answer = data.get('breedMain');
     game.answer = answer;
     game.numGuesses = 0;
-    // console.log(game.answer)
 
     generateEmptyTiles(game, data.get('breedMain').length)
 
     // Check
     const check = document.getElementById("check");
-    console.log(game);
     check.addEventListener("click", (event) => {
       const guess = document.getElementById("guess").value.toLowerCase();
-      console.log("guess " + guess)
-      console.log("answer " + answer)
-      checkBreed(event, game, answer, guess)
+      checkBreed(event, game, guess)
     });
   }
   simulate();
@@ -130,12 +99,9 @@ window.addEventListener("load", (event) => {
 function generateEmptyTiles(gameArea, ansLength) {
   const numRows = 6;
   gameHTML = ""
-  console.log(ansLength)
-  console.log("a".repeat(ansLength))
   for (let i = 0; i < numRows; i++) {
     gameHTML += generateTileRow(i, " ".repeat(ansLength))
   }
-  console.log(gameHTML)
   gameArea.innerHTML = gameHTML
 }
 
@@ -143,7 +109,6 @@ function generateTileRow(index, word) {
   row = '<div id="game-row-' + index + '">'
   for (let char of word) {
     row += generateUserTile(char)
-    console.log(char)
   }
   row += '</div>'
   return row
@@ -162,20 +127,26 @@ document.addEventListener('keydown', (event) => {
 function keyPress(key, code) {
   const guess = document.getElementById("guess");
   const game = document.getElementById("game");
+
   if (code == "Enter") {
     // try submitting guess
-    console.log(guess.value)
     res = checkBreed("", game, guess.value);
     if (res) {guess.value = ""};
-  } else {
-    guess.value = (code == "Backspace") ? guess.value.slice(0, -1) : guess.value + key.toUpperCase()
-    // value.toLowerCase();
-    // get row
-    const rowNum = 'game-row-' + game.numGuesses;
-    const row = document.getElementById(rowNum);
-    // replace row
-    row.innerHTML = generateTileRow(game.numGuesses, guess.value);
+  } else if (code == "Backspace" ) {
+    guess.value = guess.value.trim()
+    guess.value = guess.value.slice(0, -1)
+  } else if (/^\w$/.test(key)) {
+    guess.value = guess.value.trim()
+    guess.value += key.toUpperCase()
   }
+  // pad string
+  guess.value = guess.value.padEnd(game.answer.length, " ");
+  // modify doc
+  // get row
+  const rowNum = 'game-row-' + game.numGuesses;
+  const row = document.getElementById(rowNum);
+  // replace row
+  row.innerHTML = generateTileRow(game.numGuesses, guess.value);
 }
 
 function checkBreed(event, game, guess) {
@@ -190,16 +161,11 @@ function checkBreed(event, game, guess) {
   let newRow = ""
   for (let i in guess) {
     tile = generateTile(i, guess, answer);
-    // game.innerHTML += tile;
     newRow += tile;
   }
   row.innerHTML = newRow;
   game.numGuesses += 1;
-  if (guess == answer) {
-    document.getElementById("status").value = "Success!";
-  } else {
-    document.getElementById("status").value = "Fail!";
-  }
+  document.getElementById("status").value = (guess == answer) ? "Success!": "Fail!";
   return true
 }
 
